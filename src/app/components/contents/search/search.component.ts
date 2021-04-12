a5fde75b33b0ccb7a12832d4a7746ad0443e86eb
ovdncids <ovdncids@nate.com>
2021-04-12 22:58:51 +0900

    Search Component 쿼리스트링 변경과 새로고침 적용

--- src/app/components/contents/search/search.component.ts
+++ src/app/components/contents/search/search.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MembersService } from '../../../services/members.service';
 import { SearchService } from '../../../services/search.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
@@ -9,13 +10,27 @@ import { SearchService } from '../../../services/search.service';
 })
 export class SearchComponent implements OnInit {
   constructor(
+    private route: ActivatedRoute,
+    private router: Router,
     public membersService: MembersService,
     public searchService: SearchService
-  ) { }
+  ) {
+    this.route.queryParams.subscribe(queryParams => {
+      this.q = queryParams.q || ''
+      this.searchService.searchRead(this.q);
+    });
+  }
 
   q = '';
 
   ngOnInit(): void {
-    this.searchService.searchRead(this.q);
+  }
+
+  searchRead(q): void {
+    this.router.navigate(['/search'], {
+      queryParams: {
+        q: q
+      }
+    });
   }
 }
